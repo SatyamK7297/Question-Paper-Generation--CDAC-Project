04b84de0d6cb867ef9d2bc65f8e63ddf0086d98c
RiteshGhule <riteshghule222@gmail.com>
2023-02-28 16:09:27 +0530

    1st commit with pojos

---
+++ Final-Project-Code/src/main/java/com/app/FinalProjectCodeApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProjectCodeApplication.class, args);
+	}
+
+}
