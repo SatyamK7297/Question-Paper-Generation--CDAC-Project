4bcb794cf5e4be8510e8a11d84d042f0efcd8530
Smiteshchaudhary <smitesh.chaudhary@gmail.com>
2023-02-28 15:50:22 +0530

    Pojos

---
+++ Final-Project-Code/src/main/java/com/app/pojos/Question.java
@@ -0,0 +1,47 @@
+package com.app.pojos;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.Type;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Question extends BaseEntity {
+	
+	@Column(nullable = false)
+	@Type(type="text")
+	private String question;
+	
+	@Column(nullable = false)
+	private String A;
+	
+	@Column(nullable = false)
+	private String B;
+	
+	@Column(nullable = false)
+	private String C;
+	
+	@Column(nullable = false)
+	private String D;
+	
+	@Column(nullable = false)
+	private String answer;
+	
+	@ManyToOne()
+	@JoinColumn(name="subject_id")
+	private Subject subject;
+	
+	
+	
+}
